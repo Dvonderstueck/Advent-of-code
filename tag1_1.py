a28c8c0c174ea9328d3f1a5ad7455666f2814255
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2023-08-03 16:14:30 +0200

    inital commit advent of code day1
    wrong by x10

---
+++ tag1_1.py
@@ -0,0 +1,23 @@
+
+
+file_path = '/Users/dennis.vonderstueck/Ausbildung/Advent/1tag.txt'
+
+with open(file_path, 'r') as test_file:
+    
+    #lines = test_file.read().splitlines()
+    lines = test_file.readlines()
+    ZwischenErgebniss = 0
+    Highscore = 0
+
+    
+    for number in lines:
+     number = int(number.replace("\n", "0"))
+     ZwischenErgebniss = ZwischenErgebniss + number
+     if number == 0:
+        if Highscore < ZwischenErgebniss:     
+            Highscore = ZwischenErgebniss
+        ZwischenErgebniss = 0
+
+    
+      
+print(Highscore)
