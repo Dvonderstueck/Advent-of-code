3aced875d59e5aa2327300e317d3ce6c8c55aed9
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2023-08-04 10:32:40 +0200

    PART2

--- tag1_1.py
+++ tag1_1.py
@@ -1,23 +1,22 @@
-
-
 file_path = '/Users/dennis.vonderstueck/Ausbildung/Advent/1tag.txt'
 
 with open(file_path, 'r') as test_file:
-    
-    #lines = test_file.read().splitlines()
     lines = test_file.readlines()
     ZwischenErgebniss = 0
     Highscore = 0
+    meineListe = []  
 
-    
-    for number in lines:
-     number = int(number.strip()or 0 )
-     ZwischenErgebniss = ZwischenErgebniss + number
-     if number == 0:
-        if Highscore < ZwischenErgebniss:     
-            Highscore = ZwischenErgebniss
-        ZwischenErgebniss = 0
+    for line in lines:
+        number = int(line.strip() or 0)
+        ZwischenErgebniss += number
+        if number == 0:
+            if Highscore < ZwischenErgebniss:    
+                Highscore = ZwischenErgebniss
+            meineListe.append(ZwischenErgebniss)  
+            ZwischenErgebniss = 0
 
-    
-      
-print(Highscore)
+    meineListe.sort(reverse=True)  
+    last_three_highest = meineListe[:3] 
+
+print("List:", meineListe)
+print("Last three highest:", last_three_highest)
